b1571e1a6f421ae6d401cede7f1868388289c50f
Chung10Kr <coohg93@hanmail.net>
2021-10-04 16:48:40 +0900

    server setting

--- src/server.js
+++ src/server.js
@@ -1,1 +1,9 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
 console.log("!!!");
+
+app.listen(port,function(){
+    console.log(`Start Node Server Port: ${port}`)
+})
