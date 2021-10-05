388f6dcbbd122c69e1e82eac936e8d029d47e213
Chung10Kr <coohg93@hanmail.net>
2021-10-05 21:37:27 +0900

    Server, Front Setup

--- src/server.js
+++ src/server.js
@@ -2,8 +2,12 @@ import express from "express";
 
 const app = express();
 const port = 3000;
-console.log("!!!");
 
-app.listen(port,function(){
-    console.log(`Start Node Server Port: ${port}`)
-})
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*",(req, res) => res.redirect("/"));
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(port, handleListen);
